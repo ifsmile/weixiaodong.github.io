b36842a66068d0d2ec4262e0a2edc9c453160cde
weixiaodong <weixiaodongonly@163.com>
2018-01-22 13:48:07 +0800

    change theme

--- themes/miho/source/js/particles.js
+++ themes/miho/source/js/particles.js
@@ -8,7 +8,7 @@ particlesJS("particles", {
             }
         },
         "color": {
-            "value": "#00adb5"
+            "value": "#f55959"
         },
         "shape": {
             "type": "circle",
@@ -48,7 +48,7 @@ particlesJS("particles", {
         "line_linked": {
             "enable": true,
             "distance": 150,
-            "color": "#00adb5",
+            "color": "#f55959",
             "opacity": 0.4,
             "width": 1
         },
